01a524a5f2dbc78e4fb99018317110d5cd776067
Natalia Andrea Vera Torres <nataliavera2100@gmail.com>
2023-03-03 21:10:37 -0500

    TDD para debito y credito

--- src/test/java/org/example/junit5app/models/CuentaTest.java
+++ src/test/java/org/example/junit5app/models/CuentaTest.java
@@ -26,5 +26,30 @@ class CuentaTest {
         assertTrue(cuenta.getSaldo().compareTo(BigDecimal.ZERO) > 0);
     }
 
+    @Test
+    void testReferenciaCuenta() {
+        Cuenta cuenta = new Cuenta("John Doe", new BigDecimal("8900.3336"));
+        Cuenta cuenta2 = new Cuenta("John Doe", new BigDecimal("8900.3336"));
 
+        assertNotEquals(cuenta2, cuenta); //por referencia
+        assertEquals(cuenta2, cuenta); //por instancia
+    }
+
+    @Test
+    void testDebitoCuenta() {
+        Cuenta cuenta = new Cuenta("Andres", new BigDecimal("1000.12345"));
+        cuenta.debito(new BigDecimal(100));
+        assertNotNull(cuenta.getSaldo());
+        assertEquals(900, cuenta.getSaldo().intValue());
+        assertEquals("900.12345", cuenta.getSaldo().toPlainString());
+    }
+
+    @Test
+    void testCreditoCuenta() {
+        Cuenta cuenta = new Cuenta("Andres", new BigDecimal("1000.12345"));
+        cuenta.credito(new BigDecimal(100));
+        assertNotNull(cuenta.getSaldo());
+        assertEquals(1100, cuenta.getSaldo().intValue());
+        assertEquals("1100.12345", cuenta.getSaldo().toPlainString());
+    }
 }
